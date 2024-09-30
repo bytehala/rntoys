6812d4936cd64118904ddbd0a4f9ae49af17b57f
Lem <ulna-draftee-0z@icloud.com>
2024-09-29 23:54:08 -0600

    don't render the elements close to center, so it can be positioned by the parent

--- src/skia/AppleSeed.tsx
+++ src/skia/AppleSeed.tsx
@@ -4,23 +4,26 @@ import {Colors} from '../values/Colors.ts';
 import {
   Blur,
   Canvas,
-  Image,
-  useImage,
-  Group,
   Circle,
-  Mask,
-  Text as SkiaText,
   ColorMatrix,
+  Group,
+  Image,
+  Mask,
+  Shadow,
+  Text as SkiaText,
   useFont,
+  useImage,
 } from '@shopify/react-native-skia';
+import {useSharedValue} from 'react-native-reanimated';
 
 export const AppleSeed = () => {
   const image = useImage(require('../../resources/images/profile_photo.png'));
-  const {width, height} = Dimensions.get('window');
+  const {width} = Dimensions.get('window');
+  const height = width + 100;
 
   const font = useFont(require('../../resources/fonts/Roboto-Bold.ttf'), 40);
-  const text = 'Lemmy Coconut';
-  const textWidth = font?.measureText(text) || {width: 0, height: 0};
+  const nameText = 'Lemmy Coconut';
+  const nameDimensions = font?.measureText(nameText) || {width: 0, height: 0};
 
   const handleFont = useFont(
     require('../../resources/fonts/Roboto-Regular.ttf'),
@@ -32,9 +35,22 @@ export const AppleSeed = () => {
     height: 0,
   };
 
-  // center position for the profile photo
-  const centerX = (width - 256) / 2;
-  const centerY = (height - 256) / 2;
+  const imageWidth = 256;
+  const imageHeight = 256;
+
+  const bgImageDimension = width;
+
+  // offset position for the expanded image, so the shirt blur is closer to the circular clip
+  const expandedImageNegativeOffsetY = -95;
+
+  // offset position for the profile photo
+  const profilePictureOffsetX = (width - imageWidth) / 2;
+  const profilePictureOffsetY = 0;
+
+  const profilePictureCenterY = imageHeight / 2;
+  const offsetAfterProfilePicture = 150;
+
+  const nameTextY = profilePictureCenterY + offsetAfterProfilePicture;
 
   // prettier-ignore
   const brillianceMatrix = [
@@ -44,79 +60,99 @@ export const AppleSeed = () => {
     0, 0, 0, 1, 0,    // Alpha
   ];
 
+  const size = useSharedValue({width: 0, height: 0});
+
+  // NOTE: Leaving this here, because useAnimatedReaction is how you can listen to changes in reanimated SharedValue
+  // useAnimatedReaction(
+  //   () => size.value,
+  //   newSize => {
+  //     console.log('size', newSize);
+  //   },
+  //   [size],
+  // );
+
   return (
     <View style={styles.container}>
-      <View style={styles.canvasContainer}>
-        <Canvas style={{width, height}}>
+      <Canvas style={{height}} onSize={size}>
+        <Image
+          image={image}
+          fit="scaleDown"
+          x={0}
+          y={expandedImageNegativeOffsetY}
+          width={bgImageDimension}
+          height={bgImageDimension}>
+          <Blur blur={65} />
+          <ColorMatrix matrix={brillianceMatrix} />
+        </Image>
+
+        <Mask
+          mask={
+            <SkiaText
+              x={(width - nameDimensions.width) / 2}
+              y={nameTextY}
+              font={font}
+              text={nameText}
+              color="black"
+            />
+          }>
+          <Image
+            image={image}
+            fit="scaleDown"
+            x={0}
+            y={expandedImageNegativeOffsetY}
+            width={bgImageDimension}
+            height={bgImageDimension}>
+            <Blur blur={10} />
+          </Image>
+        </Mask>
+
+        <Mask
+          mask={
+            <SkiaText
+              x={(width - handleTextWidth.width) / 2}
+              y={nameTextY + nameDimensions.height}
+              font={handleFont}
+              text={handleText}
+              color="black"
+            />
+          }>
           <Image
             image={image}
             fit="contain"
             x={0}
-            y={0}
-            width={width}
-            height={height}>
-            <Blur blur={65} />
-            <ColorMatrix matrix={brillianceMatrix} />
+            y={nameDimensions.height + expandedImageNegativeOffsetY}
+            width={bgImageDimension}
+            height={bgImageDimension}>
+            <Blur blur={10} />
           </Image>
-          <Mask
-            mask={
-              <SkiaText
-                x={(width - textWidth.width) / 2}
-                y={height / 2 + 150}
-                font={font}
-                text={text}
-                color="black"
-              />
-            }>
-            <Image
-              image={image}
-              fit="contain"
-              x={(width - width * 0.9) / 2}
-              y={(height - height * 0.9) / 2}
-              width={width * 0.9}
-              height={height * 0.9}>
-              <Blur blur={10} />
-            </Image>
-          </Mask>
+        </Mask>
 
-          <Mask
-            mask={
-              <SkiaText
-                x={(width - handleTextWidth.width) / 2}
-                y={height / 2 + 150 + textWidth.height}
-                font={handleFont}
-                text={handleText}
-                color="black"
-              />
-            }>
-            <Image
-              image={image}
-              fit="contain"
-              x={(width - width * 0.8) / 2}
-              y={(height - height * 0.8) / 2}
-              width={width}
-              height={height}>
-              <Blur blur={10} />
-            </Image>
-          </Mask>
-
-          <Mask
-            mask={
-              <Group>
-                <Circle cx={width / 2} cy={height / 2} r={112} />
-              </Group>
-            }>
-            <Image
-              image={image}
-              fit="contain"
-              x={centerX}
-              y={centerY}
-              width={256}
-              height={256}
-            />
-          </Mask>
-        </Canvas>
-      </View>
+        {/* Profile Picture Circle */}
+        <Circle cx={width / 2} cy={profilePictureCenterY} r={112}>
+          <Shadow
+            dx={0}
+            dy={5}
+            blur={10}
+            color={'rgba(0,0,0,0.5)'}
+            shadowOnly={true}
+          />
+        </Circle>
+        <Mask
+          mask={
+            <Group>
+              <Circle cx={width / 2} cy={profilePictureCenterY} r={112} />
+            </Group>
+          }>
+          <Image
+            image={image}
+            fit="contain"
+            x={profilePictureOffsetX}
+            y={profilePictureOffsetY}
+            width={imageWidth}
+            height={imageHeight}
+          />
+        </Mask>
+      </Canvas>
     </View>
   );
 };
@@ -125,7 +161,7 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     width: '100%',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: Colors.tea_green.DEFAULT,
   },
   canvasContainer: {

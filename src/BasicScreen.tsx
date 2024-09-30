24aeb3155247132b947002c39a503ea37b065e5c
Lem <ulna-draftee-0z@icloud.com>
2024-09-29 18:54:24 -0600

    Create a basic screen to start

---
+++ src/BasicScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Colors} from './resources/Colors.ts';
+
+export const BasicScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Basic Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: Colors.tea_green.DEFAULT,
+  },
+});
